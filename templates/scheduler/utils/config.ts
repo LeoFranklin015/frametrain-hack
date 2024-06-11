d363be4a3ab7bac2752f41dbf90b407bbf12b2df
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2024-06-12 02:06:57 +0530

    updated

--- templates/scheduler/utils/config.ts
+++ templates/scheduler/utils/config.ts
@@ -2,13 +2,15 @@ import { createWalletClient, http } from 'viem'
 import { privateKeyToAccount } from 'viem/accounts'
 import { baseSepolia } from 'viem/chains'
 
-const privateKey = process.env.NEXT_PUBLIC_KEY
-if (!privateKey) {
-    throw new Error('Private key is not defined')
-}
-console.log('private key : ' + privateKey)
+// const privateKey = process.env.NEXT_PUBLIC_KEY
+// if (!privateKey) {
+//     throw new Error('Private key is not defined')
+// }
+// console.log('private key : ' + privateKey)
 
-const account = privateKeyToAccount(privateKey as `0x${string}`)
+const account = privateKeyToAccount(
+    '0xd6912910ad4b09bb62377b0e3c0681bc1f030e15fe3c8d4e0ffb2832dae8b1ec' as `0x${string}`
+)
 
 export const client = createWalletClient({
     account,
